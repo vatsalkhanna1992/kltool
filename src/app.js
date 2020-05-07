04df96c30f85bc64b500e25393bd0e785cc7985b
Vatsal Khanna <vatsalkhanna1992@gmail.com>
2020-05-07 21:07:11 +0530

    Adds db connection and registration form.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const hbs = require('hbs')
 const path = require('path')
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
+const Users = require('./models/users')
+require('./utils/dbconnect')
 
 const app = express()
 const port = process.env.PORT || 3002
@@ -11,13 +13,13 @@ const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup static directory to use.
 app.use(express.static(publicDirectoryPath))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/js', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/js')))
 app.use('/js', express.static(path.join(__dirname, '../node_modules/jquery/dist')))
 app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')))
 app.use('/js', express.static(path.join(__dirname, '../node_modules/bootstrap-material-design/dist/js')))
 app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap-material-design/dist/css')))
-// parse application/x-www-form-urlencoded
-//app.use(bodyParser.urlencoded({ extended: true }))
 
 // Set views engine to use handlebars.
 app.set('view engine', 'hbs')
@@ -31,14 +33,32 @@ app.get('', (req, res) => {
 })
 
 app.post('/dashboard', (req, res) => {
-    console.log(req.body.username)
-});
+    console.log(req.body)
+})
 
-const validator = () => {
-    return console.log('Validator.')
-}
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.post('/user/registration', (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    const user = new Users({
+        username: req.body.username,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        password: req.body.password,
+    })
+    user.save().then(() => {
+        res.render('dashboard', {
+            firstName: req.body.first_name,
+            lastName: req.body.last_name
+        })
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
 
 // Start server.
 app.listen(port, () => {
-    console.log('Server is up and running at port '+ port)
+    console.log('Server is up and running at port ' + port)
 })
---
+++ src/models/users.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const Users = mongoose.model('Users', {
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Please enter valid email.')
+            }
+        }
+    },
+    first_name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    last_name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Enter valid password.')
+            }
+        }
+    }
+})
+
+module.exports = Users;
