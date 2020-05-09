574cf2e5d0d266936a23697f57e684d59a7dcd4d
Vatsal Khanna <vatsalkhanna1992@gmail.com>
2020-05-09 17:55:32 +0530

    Fix login issue.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,13 +4,28 @@ const Users = require('../models/users')
 const auth = async (req, res, next) => {
     try {
         const token = await getJwtToken(req)
+        if (!token) {
+            if (req.path !== '/') {
+                return res.render('index', {
+                    error: 'Please authenticate.'
+                })
+            } else {
+                return res.render('index')
+            }
+        }
         // If you don't use cookie-parser then by default cookies are available at req.headers.cookie.
         //const token = req.header('Authorization').replace('Bearer ', '')
         const decode = jwt.verify(token, process.env.JWT_SECRET)
         const user = await Users.findOne({ _id: decode._id, 'tokens.token': token })
 
-        if (!user && req.path !== '/') {
-            return res.redirect('/')
+        if (!user) {
+            if (req.path !== '/') {
+                return res.render('index', {
+                    error: 'Please authenticate.'
+                })
+            } else {
+                return res.render('index')
+            }
         }
         req.token = token
         req.user = user
